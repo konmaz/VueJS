c7cdff463b60c57ead03ad54d47b7279354a3d08
Konstantinos Mazgaltzidis <kamazgal@csd.auth.gr>
2023-10-02 09:34:39 +0300

    Just checking VueJS by creating a simple page with forms

---
+++ src/components/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav
+      :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Vue</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li v-for="(page, index) in publishedPages" class="nav-item" :key="index">
+          <navbar-link
+              :page="page"
+              :isActive="activePage === index"
+              @click.prevent="navLinkClick(index)"
+
+          ></navbar-link>
+
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button
+            class="btn btn-primary"
+            @click.prevent="changeTheme()"
+        >Toggle Navbar
+        </button>
+      </form>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+import NavbarLink from "@/components/NavbarLink.vue";
+export default {
+  components:{
+    NavbarLink
+  },
+  props: ['pages', 'activePage', 'navLinkClick'],
+  created() {
+    this.getThemeSetting();
+  },
+  computed:{
+    publishedPages(){
+      return this.pages.filter(p => p.published)
+    }
+  },
+  data() {
+    return {
+      theme: 'light',
+    }
+  },
+  methods: {
+    changeTheme() {
+      let theme = 'light';
+      if (this.theme === 'light') {
+        theme = 'dark';
+      }
+
+      this.theme = theme;
+      this.storeThemeSetting();
+    },
+    storeThemeSetting(){
+      localStorage.setItem('theme',this.theme);
+    },
+    getThemeSetting(){
+      let theme = localStorage.getItem('theme');
+      if (theme)
+        this.theme = theme;
+    }
+  }
+}
+</script>
